bbc15244ac1b8650ca097336c715c2a4c7663b3e
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-05-02 21:39:29 +0300

    fix form

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -27,12 +27,12 @@ class UserLoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    username = forms.CharField(max_length=20)
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password here'}))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
+    username = forms.CharField(max_length=20, label='', attrs={'placeholder': 'Username'})
+    first_name = forms.CharField(max_length=20, label='', attrs={'placeholder': 'First name'})
+    last_name = forms.CharField(max_length=20, label='', attrs={'placeholder': 'Last name'})
+    email = forms.EmailField(label='', attrs={'placeholder': 'Email'})
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Enter password here'}))
+    confirm_password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
     class Meta:
         model = User
