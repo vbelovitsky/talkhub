29042bc8e09ba7cda7e13dc47ddd1e4e18cbec7a
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-05-03 20:46:56 +0300

    gmail

--- talkhub/settings.py
+++ talkhub/settings.py
@@ -128,13 +128,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "talkhub@yandex.ru"
-EMAIL_HOST_PASSWORD = "talk321hub"
 EMAIL_USE_TLS = True
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+EMAIL_HOST_USER = "talkhubproject@gmail.com"
+EMAIL_HOST_PASSWORD = "kqngsjkdtqteyzrm"
+
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- talkhub/urls.py
+++ talkhub/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.conf.urls import url, include
 from blogapp import views
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = [
