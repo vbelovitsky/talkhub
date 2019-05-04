84a9078152436bbacebe31bb6a221ecf182fff19
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-05-04 10:19:51 +0300

    fix tags

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils.text import slugify
 
 
 class Post(models.Model):
