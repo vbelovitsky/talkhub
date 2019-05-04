c5ec0c3c808ac31f620f055aeedea845b0cec1c1
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-05-05 02:02:57 +0300

    fix

--- talkhub/settings.py
+++ talkhub/settings.py
@@ -185,3 +185,4 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
