a3a455fa683f228a89793b7a0ec7e7be204f52e5
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-05-05 12:22:08 +0300

    stupid fix

--- talkhub/settings.py
+++ talkhub/settings.py
@@ -174,7 +174,7 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.linkedin.LinkedinOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.vk.VKOAuth2',
-    'social_core.backends.yandex.YandexOAuth2'
+    'social_core.backends.yandex.YandexOAuth2',
     
     'django.contrib.auth.backends.ModelBackend',
 ]
